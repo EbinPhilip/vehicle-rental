69bf5e52d8149e16c85217b2881fbfa14d8e25dc
Ebin <stargazer18@gmail.com>
2022-08-28 14:55:28 +0530

    Fixed booking

--- src/main/java/com/ebin/vehiclerental/commands/BookCommand.java
+++ src/main/java/com/ebin/vehiclerental/commands/BookCommand.java
@@ -1,5 +1,6 @@
 package com.ebin.vehiclerental.commands;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import com.ebin.vehiclerental.services.BookingService;
@@ -23,7 +24,8 @@ public class BookCommand implements Command {
             int endTime = Integer.valueOf(tokens.get(4).trim());
 
             double price = bookingService.bookVehicle(branchId, vehicleType, startTime, endTime);
-            System.out.println(price);
+            DecimalFormat format = new DecimalFormat("0.#");
+            System.out.println(format.format(price));
         } catch (RuntimeException e) {
             System.out.println("-1");
         }
